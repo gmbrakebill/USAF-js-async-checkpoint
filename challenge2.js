ece2670e581540a76c300d3df66ff89698684d9e
gmbrakebill <64878965+gmbrakebill@users.noreply.github.com>
2020-06-07 20:48:26 -0500

    [check]

---
+++ challenge2.js
@@ -0,0 +1,9 @@
+console.log("hello world")
+console.log(typeof undefined); // we have left off the label for this message, but will include it for the rest
+console.log('type of true:', typeof true);
+console.log('type of false:', typeof false);
+console.log('type of 9', typeof 9);
+console.log('type of 4.0:', typeof 4.0);
+console.log('type of "a string of characters":', typeof "a string of characters");
+console.log('Is an array?:', Array.isArray([3, 4, 5]));
+console.log('Is an array?:', Array.isArray({a: 1}));
